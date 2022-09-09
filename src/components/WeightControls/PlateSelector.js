572075220e24c33d4923c9d479c3f56eaff2d3cd
Phillip <phillip.lam0@gmail.com>
2022-09-09 12:49:49 -0400

    style: emphasizing selected plate

--- src/components/WeightControls/PlateSelector.js
+++ src/components/WeightControls/PlateSelector.js
@@ -36,11 +36,15 @@ export default function PlateSelector(props) {
         else {
             setSelectedClass('');
         }
-    }, [plateAmount.plates, props.id])
+    }, [plateAmount.plates, props.id]);
 
     return (
         <Badge badgeContent={count} color="primary">
-            <button id={`${props.id}`} className={`plate plate--button ${props.plateClass} ${selectedClass}`} onClick={plateClickHandler}>
+            <button
+                id={`${props.id}`}
+                className={`plate plate--button ${props.plateClass} ${selectedClass} ${selectedClass ? '' : 'plate--button-unselected'}`}
+                onMouseDown={plateClickHandler}
+                onClick={props.increment}>
                 {props.weight}
             </button>
         </Badge>
