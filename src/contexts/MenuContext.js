2403db1bf2ef163a537962c928d74bd684ce7bd0
Phillip <phillip.lam0@gmail.com>
2022-09-26 12:08:07 -0400

    fix: menu profiles not loading in localStorage data on browser refresh (BC-39)

--- src/contexts/MenuContext.js
+++ src/contexts/MenuContext.js
@@ -2,6 +2,17 @@ import React, { useState, createContext, useEffect } from 'react';
 import { INITIAL_WEIGHT_PROFILE, INITIAL_TOTAL_WEIGHT } from './WeightContext';
 import getEmoji from '../helper/emojiRandomizer';
 
+const INITIAL_MENU_ITEMS = {
+    menuItems:
+        [
+            {
+                key: 0,
+                profileTitle: getEmoji() + INITIAL_WEIGHT_PROFILE.profileTitle,
+                weight: INITIAL_TOTAL_WEIGHT,
+            },
+        ]
+}
+
 export const MenuContext = createContext();
 
 export const MenuProvider = props => {
@@ -13,15 +24,30 @@ export const MenuProvider = props => {
     }, [setProfiles])
 
     // States
-    const [menus, setMenus] = useState({
-        menuItems:
-            [
-                {
-                    key: 0,
-                    profileTitle: getEmoji() + INITIAL_WEIGHT_PROFILE.profileTitle,
-                    weight: INITIAL_TOTAL_WEIGHT,
-                },
-            ]
+    const [menus, setMenus] = useState(() => {
+        const storage = localStorage.getItem('profile1');
+        if (storage) {
+            let menuItems = [];
+            let updatedMenu = { menuItems };
+
+            // parse each key value
+            for (let i = 0; i < localStorage.length; i++) {
+                const profileKey = localStorage.key(i);
+                const localStorageProfile = JSON.parse(localStorage.getItem(profileKey));
+
+                // extract key, profileTitle, and totalWeight from key value
+                menuItems[i] = {
+                    key: i,
+                    profileTitle: localStorageProfile.profileTitle,
+                    weight: localStorageProfile.totalWeight
+                }
+            }
+
+            return updatedMenu;
+        }
+        else {
+            return INITIAL_MENU_ITEMS;
+        }
     });
 
     return (
